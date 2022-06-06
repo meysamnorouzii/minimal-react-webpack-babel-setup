aa4e89ba8ae7900777dd6b2c329c077b4488f5f6
meysamnorouzii <work.meysamnorouzi@gmail.com>
2022-06-06 13:19:52 +0430

    setup html-webpack-plugin
    create /public/index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -22,4 +23,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
 };
