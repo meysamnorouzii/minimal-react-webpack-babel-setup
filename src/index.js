dc8fbdd30cf10378fcd3e9e58f55a80919557f3a
meysamnorouzii <work.meysamnorouzi@gmail.com>
2022-06-06 17:42:28 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
