4c248902282c7f5c3e835a22063695216859e994
meysamnorouzii <work.meysamnorouzi@gmail.com>
2022-06-06 12:56:56 +0430

    initial babel setup
    create babel.config.js  and
    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,18 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
