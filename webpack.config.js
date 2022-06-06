5bb9b8dd6219978099cca2809aed255f96c86842
meysamnorouzii <work.meysamnorouzi@gmail.com>
2022-06-06 12:39:34 +0430

    initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
